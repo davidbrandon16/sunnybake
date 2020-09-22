1282afab47d79b6c28740744056cefb1126c4017
david_brandon <tanihubgroup@tanihubgroups-MacBook-Pro.local>
2020-09-22 15:42:49 +0700

    add payment and product

--- Controller/ReportController.go
+++ Controller/ReportController.go
@@ -22,6 +22,7 @@ func (reportCon ReportCon) View(ctx *gin.Context){
 
 func (reportCon ReportCon) Generate(ctx *gin.Context){
 	var transactionHeaders []Model.TransactionHeader
+	var products []Model.Product
 	start_date := ctx.PostForm("start_date")
 	end_date := ctx.PostForm("end_date")
 	xlsx := excelize.NewFile()
@@ -30,39 +31,55 @@ func (reportCon ReportCon) Generate(ctx *gin.Context){
 	xlsx.SetCellValue("Sheet1", "C1", "Price")
 	xlsx.SetCellValue("Sheet1", "D1", "Delivery Cost")
 	xlsx.SetCellValue("Sheet1", "E1", "Discount")
-	xlsx.SetCellValue("Sheet1", "F1", "Product")
-	xlsx.SetCellValue("Sheet1", "G1", "Price Per Item")
-	xlsx.SetCellValue("Sheet1", "H1", "Quantity")
+	xlsx.SetCellValue("Sheet1", "F1", "Payment Bank")
+	xlsx.SetCellValue("Sheet1", "G1", "Payment Account Name")
+	xlsx.SetCellValue("Sheet1", "H1", "Payment Date")
+	xlsx.SetCellValue("Sheet1", "I1", "Payment Price")
 	db, err := Connect()
-	// err = db.Get(&transactionHeader, "SELECT id,customername,customeraddress,discount,deliverycost,price,orderdate,phonenumber, delivery,concat(substring(senddatetime, 1,position (':' in senddatetime )-3),' ',right(senddatetime ,4),' ',substring(senddatetime , position (':' in senddatetime )-2 , 8)) as senddatetime FROM transactionheader WHERE id = $1", id)
-	err = db.Select(&transactionHeaders, "SELECT id,customername,customeraddress,discount,deliverycost,price,orderdate,phonenumber, delivery FROM transactionheader WHERE DATE(orderdate) BETWEEN DATE($1) AND DATE($2)", start_date, end_date)
-	if err != nil {
+	var mapProduct map[int]int
+	mapProduct = make(map[int]int)
+	err = db.Select(&products,"SELECT * FROM products")
+	if err != nil{
 		fmt.Println(err.Error())
-	}else{
-		var row int =2 
-		for _, transactionHeader := range transactionHeaders{
-			xlsx.SetCellValue("Sheet1", fmt.Sprintf("A%d",row), transactionHeader.OrderDate)
-			xlsx.SetCellValue("Sheet1", fmt.Sprintf("B%d",row), transactionHeader.CustomerName)
-			xlsx.SetCellValue("Sheet1", fmt.Sprintf("C%d",row), transactionHeader.Price)
-			xlsx.SetCellValue("Sheet1", fmt.Sprintf("D%d",row), transactionHeader.DeliveryCost)
-			xlsx.SetCellValue("Sheet1", fmt.Sprintf("E%d",row), transactionHeader.Discount)
-			var transactionDetails []Model.TransactionDetail
-			err = db.Select(&transactionDetails, "SELECT * FROM transactiondetail WHERE transaction_header_id = $1", transactionHeader.Id)
-			if err != nil {
-				fmt.Println(err.Error())
-			} else {
-				for _, transactionDetail := range transactionDetails {
-					var product Model.Product
-					err = db.Get(&product, "SELECT * FROM products WHERE id = $1", transactionDetail.Product_id)
+	}else {
+		var column int = 74
+		for _,product := range products{
+			mapProduct[product.Id] = column
+			xlsx.SetCellValue("Sheet1", fmt.Sprintf("%c1",column), product.Name)
+			column++
+		}
+		
+		// err = db.Get(&transactionHeader, "SELECT id,customername,customeraddress,discount,deliverycost,price,orderdate,phonenumber, delivery,concat(substring(senddatetime, 1,position (':' in senddatetime )-3),' ',right(senddatetime ,4),' ',substring(senddatetime , position (':' in senddatetime )-2 , 8)) as senddatetime FROM transactionheader WHERE id = $1", id)
+		err = db.Select(&transactionHeaders, "SELECT id,customername,customeraddress,discount,deliverycost,price,orderdate,phonenumber, delivery FROM transactionheader WHERE DATE(orderdate) BETWEEN DATE($1) AND DATE($2)", start_date, end_date)
+		if err != nil {
+			fmt.Println(err.Error())
+		}else{
+			var row int =2 
+			for _, transactionHeader := range transactionHeaders{
+				xlsx.SetCellValue("Sheet1", fmt.Sprintf("A%d",row), transactionHeader.OrderDate)
+				xlsx.SetCellValue("Sheet1", fmt.Sprintf("B%d",row), transactionHeader.CustomerName)
+				xlsx.SetCellValue("Sheet1", fmt.Sprintf("C%d",row), transactionHeader.Price)
+				xlsx.SetCellValue("Sheet1", fmt.Sprintf("D%d",row), transactionHeader.DeliveryCost)
+				xlsx.SetCellValue("Sheet1", fmt.Sprintf("E%d",row), transactionHeader.Discount)
+				var transactionDetails []Model.TransactionDetail
+				err = db.Select(&transactionDetails, "SELECT * FROM transactiondetail WHERE transaction_header_id = $1", transactionHeader.Id)
+				if err != nil {
+					fmt.Println(err.Error())
+				} else {
+					for _, transactionDetail := range transactionDetails {	
+						xlsx.SetCellValue("Sheet1", fmt.Sprintf("%c%d",mapProduct[transactionDetail.Product_id],row),transactionDetail.Qty )
+					}
+					var payment Model.Payment
+					err = db.Get(&payment,"SELECT * FROM payment WHERE transactionheader_id=$1",transactionHeader.Id)
 					if err != nil {
 						fmt.Println(err.Error())
-					} else {
-						xlsx.SetCellValue("Sheet1", fmt.Sprintf("F%d",row), product.Name)
-						xlsx.SetCellValue("Sheet1", fmt.Sprintf("G%d",row), product.Price)
-						xlsx.SetCellValue("Sheet1", fmt.Sprintf("H%d",row), transactionDetail.Qty)
-						row++
 					}
+					xlsx.SetCellValue("Sheet1", fmt.Sprintf("F%d",row),payment.BankName )
+					xlsx.SetCellValue("Sheet1", fmt.Sprintf("G%d",row),payment.AccountName )
+					xlsx.SetCellValue("Sheet1", fmt.Sprintf("H%d",row),payment.Date )
+					xlsx.SetCellValue("Sheet1", fmt.Sprintf("I%d",row),payment.Price )
 				}
+				row++
 			}
 		}
 	}
